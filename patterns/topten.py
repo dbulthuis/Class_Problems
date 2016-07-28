bd13906d0438af83e7594dbd3482fd3c0690d89e
dbulthuis <dbulthuis@gmail.com>
2016-07-28 12:20:45 -0500

    initial version

---
+++ patterns/topten.py
@@ -0,0 +1,40 @@
+import sys
+import csv
+
+
+def mapper():
+    reader = csv.reader(sys.stdin, delimiter='\t')
+    writer = csv.writer(sys.stdout, delimiter='\t', quotechar='"', quoting=csv.QUOTE_ALL)
+    sorted_dict = {}
+
+    for line in reader:
+        sorted_dict[len(line[4])] = line
+
+    top_ten = sorted(sorted_dict.keys(),reverse=True)
+    for x in range(10):
+        writer.writerow(sorted_dict[top_ten[9-x]])
+
+
+test_text = """\"\"\t\"\"\t\"\"\t\"\"\t\"333\"\t\"\"
+\"\"\t\"\"\t\"\"\t\"\"\t\"88888888\"\t\"\"
+\"\"\t\"\"\t\"\"\t\"\"\t\"1\"\t\"\"
+\"\"\t\"\"\t\"\"\t\"\"\t\"11111111111\"\t\"\"
+\"\"\t\"\"\t\"\"\t\"\"\t\"1000000000\"\t\"\"
+\"\"\t\"\"\t\"\"\t\"\"\t\"22\"\t\"\"
+\"\"\t\"\"\t\"\"\t\"\"\t\"4444\"\t\"\"
+\"\"\t\"\"\t\"\"\t\"\"\t\"666666\"\t\"\"
+\"\"\t\"\"\t\"\"\t\"\"\t\"55555\"\t\"\"
+\"\"\t\"\"\t\"\"\t\"\"\t\"999999999\"\t\"\"
+\"\"\t\"\"\t\"\"\t\"\"\t\"7777777\"\t\"\"
+"""
+
+
+# This function allows you to test the mapper with the provided test string
+def main():
+    import StringIO
+    sys.stdin = StringIO.StringIO(test_text)
+    mapper()
+    sys.stdin = sys.__stdin__
+
+
+main()
